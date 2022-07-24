3659f2996d32262e19c73e5bc7ea8e5e5bb4c9d0
skassaye <novomira@yahoo.com>
2022-07-23 22:38:35 -0700

    Added home, about me, skills, portfolio and contact me

---
+++ index.js
@@ -0,0 +1,12 @@
+// const primaryNav = document.getElementById("primary-navigation");
+// const navToggle= document.querySelector(".mobile-nav-toggle");
+// const myFunc = ()=> {
+//     const visibility= primaryNav.getAttribute('data-visible');
+//     if (visibility === "false"){
+//         primaryNav.setAttribute('data-visible', true);
+//     } else if (visibility=== "true"){
+//         primaryNav.setAttribute('data-visible', false);
+//     }
+// }
+
+// navToggle.addEventListener("click", myFunc); 
